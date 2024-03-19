f5c4add042de211eb0591ab3a6bf4ad0fe4c963c
unknown <jorgeluisandrade300@gmail.com>
2024-03-19 12:10:49 -0500

    Clase 19/03/2024

--- app_web/academics/models.py
+++ app_web/academics/models.py
@@ -12,6 +12,8 @@ class User(models.Model):
     updated_at = models.DateTimeField(default=datetime.datetime.now ())
     created_at = models.DateTimeField(default=datetime.datetime.now ())
     deleted_at = models.DateTimeField(null=True, blank=True)
+    def __str__(self):
+        return self.email
 
 class Person(models.Model):
     firstname = models.CharField(max_length=20)
