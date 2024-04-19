c39a1ca938a92a1c234e3bc0d169c587fc26ac06
unknown <jorgeluisandrade300@gmail.com>
2024-04-19 17:07:56 -0500

    clase 19/04/2024

--- app_web/academics/views.py
+++ app_web/academics/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse(" ::Welcome to my site ::")
+    return HttpResponse(" ::Welcome to my site  ::")
 
 def list_persons(request):
     return HttpResponse(" here you find a list of people madafker")
